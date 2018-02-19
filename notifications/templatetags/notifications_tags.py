7fd17aae1cf6a04539a620e7404395d4183f1b49
Lens Hunnel <mbinalens@gmail.com>
2018-02-19 14:07:56 +0100

    Update notifications_tags.py

--- notifications/templatetags/notifications_tags.py
+++ notifications/templatetags/notifications_tags.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-from django.core.urlresolvers import reverse
 from django.template import Library
+from django.urls import reverse
 from django.utils.html import format_html
 
 register = Library()
 
 
-@register.assignment_tag(takes_context=True)
+@register.simple_tag(takes_context=True)
 def notifications_unread(context):
     user = user_context(context)
     if not user:
@@ -16,8 +16,8 @@ def notifications_unread(context):
 
 # Requires vanilla-js framework - http://vanilla-js.com/
 @register.simple_tag
-def register_notify_callbacks(badge_class='live_notify_badge',
-                              menu_class='live_notify_list',
+def register_notify_callbacks(badge_id='live_notify_badge',
+                              menu_id='live_notify_list',
                               refresh_period=15,
                               callbacks='',
                               api_name='list',
@@ -31,16 +31,16 @@ def register_notify_callbacks(badge_class='live_notify_badge',
     else:
         return ""
     definitions = """
-        notify_badge_class='{badge_class}';
-        notify_menu_class='{menu_class}';
+        notify_badge_id='{badge_id}';
+        notify_menu_id='{menu_id}';
         notify_api_url='{api_url}';
         notify_fetch_count='{fetch_count}';
         notify_unread_url='{unread_url}';
         notify_mark_all_unread_url='{mark_all_unread_url}';
         notify_refresh_period={refresh};
     """.format(
-        badge_class=badge_class,
-        menu_class=menu_class,
+        badge_id=badge_id,
+        menu_id=menu_id,
         refresh=refresh_period,
         api_url=api_url,
         unread_url=reverse('notifications:unread'),
@@ -56,20 +56,20 @@ def register_notify_callbacks(badge_class='live_notify_badge',
 
 
 @register.simple_tag(takes_context=True)
-def live_notify_badge(context, badge_class='live_notify_badge'):
+def live_notify_badge(context, badge_id='live_notify_badge', classes=""):
     user = user_context(context)
     if not user:
         return ''
 
-    html = "<span class='{badge_class}'>{unread}</span>".format(
-        badge_class=badge_class, unread=user.notifications.unread().count()
+    html = "<span id='{badge_id}' class='{classes}'>{unread}</span>".format(
+        badge_id=badge_id, classes=classes, unread=user.notifications.unread().count()
     )
     return format_html(html)
 
 
 @register.simple_tag
-def live_notify_list(list_class='live_notify_list'):
-    html = "<ul class='{list_class}'></ul>".format(list_class=list_class)
+def live_notify_list(list_id='live_notify_list', classes=""):
+    html = "<ul id='{list_id}' class='{classes}'></ul>".format(list_id=list_id, classes=classes)
     return format_html(html)
 
 
